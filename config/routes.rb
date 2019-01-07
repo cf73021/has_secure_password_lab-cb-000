8db9f8f59bae73f4d81a5d8c4de2032b77db511f
Chris Franklin <cf73021@gmail.com>
2019-01-07 03:37:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 post 'sessions', to: 'sessions#create'
-get 'signup', to: 'users#new'
+get '/signup', to: 'users#new'
 post 'users', to: 'users#create'
 get 'login', to: 'sessions#new'
 get 'welcome', to: 'users#welcome'
